5ce070d1a02d37429ebf21ebbd94a59ec3ffb111
JerryWang <jerry002@126.com>
2016-06-05 22:53:16 +0800

    生成用户登录令牌

--- src/main/java/com/zouzoutingting/utils/TokenUtil.java
+++ src/main/java/com/zouzoutingting/utils/TokenUtil.java
@@ -11,11 +11,20 @@ public class TokenUtil {
 	
 	/**
 	 * 生成用户登录令牌，长度为32位
-	 * 具体生成公式为：Token = md5(md5(userid)+time(long)+md5(randomStr+finalKey))
 	 * @return
 	 */
-	public static String genToken(long uid)  {
-		return MD5.Md5Encryt(MD5.Md5Encryt(Long.toString(uid)) + System.currentTimeMillis() + MD5.Md5Encryt(Math.random() + Global.DESKEY));
+	public static String generateToken(long uid)  {
+		return MD5.Md5Encryt(MD5.Md5Encryt(Long.toString(uid)) + MD5.Md5Encryt(Global.DESKEY));
 	}
 	
+	public static boolean checkToken(long uid, String token) {
+		if(token == null) {
+			return false;
+		}
+		
+		if(TokenUtil.generateToken(uid).equals(token)) {
+			return true;
+		}
+		return false;
+	}
 }	
