548073391724a8918dfb5c34dbc8b8c77d7bd4c1
JerryWang <jerry002@126.com>
2016-07-13 00:59:07 +0800

    OssApi

---
+++ src/main/java/com/zouzoutingting/api/OssApi.java
@@ -0,0 +1,13 @@
+package com.zouzoutingting.api;
+
+/**
+ * @author Jerry Wang
+ * @Email  jerry002@126.com
+ * @date   2016年6月22日
+ */
+public class OssApi {
+	
+	private final static String ACCESS_KEY_ID = "nfWRH14XbUZL8xV3";
+	private final static String ACCESS_KEY_SECRET = "YGmjR0KtRPRF4OwK25Y3jQbzUZ7GdJ";
+	
+}
