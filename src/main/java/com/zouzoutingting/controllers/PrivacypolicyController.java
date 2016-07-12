ac80f75da6e8a03d043a84b6bf868f913b784cae
wangjiajun <jerry002@126.com>
2016-07-12 11:54:32 +0800

    privacypolicy

---
+++ src/main/java/com/zouzoutingting/controllers/PrivacypolicyController.java
@@ -0,0 +1,24 @@
+package com.zouzoutingting.controllers;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * @author Wangjiajun 
+ * @Email  wangjiajun@58.com
+ * @date   2016年7月11日
+ */
+@Controller
+public class PrivacypolicyController extends BaseController {
+	
+	@RequestMapping(value = "/privacypolicy", method = RequestMethod.GET)
+	public ModelAndView privacypolicy(ModelAndView model, HttpServletRequest request, HttpServletResponse response) {
+		model.setViewName("forward:privacypolicy.jsp");
+		return model;
+	}
+}
