62826ec4b488faf474ca77b543a7578c82e4211c
wangjiajun <jerry002@126.com>
2016-08-26 13:40:31 +0800

    修改spring boot版本号

--- src/main/java/com/zouzoutingting/ApplicationMain.java
+++ src/main/java/com/zouzoutingting/ApplicationMain.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * 启动Spring Boot项目的唯一入口
@@ -21,4 +24,14 @@ public class ApplicationMain {
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(ApplicationMain.class, args);
 	}
+	
+	/*@RequestMapping(value ="/hello", method = RequestMethod.GET)
+    @ResponseBody
+    public String hello(){
+        return "hello world";
+    }
+ 
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationMain.class, args);
+    }*/
 }
